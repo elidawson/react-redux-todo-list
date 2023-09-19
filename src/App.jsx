774b1d6cfe1ff6f53516edf1c384a64d6657ec09
Eli Dawson <edawson@alumni.berklee.edu>
2023-09-19 12:47:46 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodoForm from './components/AddTodoForm';
+import TodoList from './components/TodoList';
+import TotalCompleteItems from './components/TotalCompleteItems';
+
+const App = () => {
+	return (
+		<div>
+			<h1>My Todo List</h1>
+			<AddTodoForm />
+			<TodoList />
+			<TotalCompleteItems />
+		</div>
+	);
+};
+
+export default App;
